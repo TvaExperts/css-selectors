9c090e853973648d090d78867eb0cd13b422279d
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-06-17 15:27:10 +0300

    chore: add webpack, eslint, ts, scss, prettier

---
+++ rs-css/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  plugins: ['prettier', 'import', '@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['.eslintrc.js', 'webpack.config.js'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'import/extensions': 'off',
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'max-lines-per-function': ['error', 40],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+  },
+};
---
+++ rs-css/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  entry: {
+    index: './src/index',
+  },
+  output: {
+    filename: '[name].[contenthash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: path.join('assets', '[name].[contenthash:8][ext]'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('assets', 'icons', '[name].[contenthash:8][ext]'),
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    open: true,
+    port: 9000,
+  },
+};

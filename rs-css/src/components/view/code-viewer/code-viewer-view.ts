eefcd7d2d3720d49c499a040b7968c7fb92eb017
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-07-01 00:18:30 +0300

    fix: fix code highlighting

--- rs-css/src/components/view/code-viewer/code-viewer-view.ts
+++ rs-css/src/components/view/code-viewer/code-viewer-view.ts
@@ -2,11 +2,10 @@ import './code.scss';
 
 import { ViewParams } from '../types';
 import View from '../view';
-import { GameHTMLTag } from '../../../data/levels';
-import ElementParams from '../../util/types';
 import ElementCreator from '../../util/element-creator';
-import { CssClasses, Constants } from './types';
+import { GameHTMLTag } from '../../../data/levels';
 import { getHighlightedTags } from '../../util/highlight-js';
+import { CssClasses, Constants } from './types';
 
 export default class CodeViewerView extends View {
   elements: Map<string, HTMLElement>;
@@ -37,17 +36,10 @@ export default class CodeViewerView extends View {
     this.viewElementCreator.addInnerElement(this.codeBlock);
   }
 
-  private generateLineNumbersText(): string {
-    const numbers: string[] = [];
-    for (let i = 1; i <= Constants.LINES_COUNT; i += 1) {
-      numbers.push(i.toString());
-    }
-    return numbers.join('<br>');
-  }
-
   public setHoverListeners(callback: (signElement: string) => void) {
     this.getHtmlElement().addEventListener('mouseover', (e) => {
       let { target }: { target: EventTarget | null } = e;
+
       while (target !== this.getHtmlElement()) {
         if (!target || !(target instanceof Element)) return;
         if (target.tagName !== 'SPAN') {
@@ -62,6 +54,7 @@ export default class CodeViewerView extends View {
         target = target.parentNode;
       }
     });
+
     this.getHtmlElement().addEventListener('mouseleave', () => callback(''));
   }
 
@@ -70,19 +63,83 @@ export default class CodeViewerView extends View {
     this.buildHTMLCode(markup);
   }
 
+  public showSelectedItem(signElement: string): void {
+    this.removeSelection();
+    if (!signElement) return;
+
+    const element = this.elements.get(signElement);
+    if (!element) return;
+    const nodes = element.children;
+    for (let i = 0; i < nodes.length; i += 1) {
+      if (nodes[i].tagName === 'SPAN') {
+        nodes[i].classList.add(CssClasses.SELECTED_LINE);
+      }
+    }
+  }
+
   public getSignsElementBySelector(selector: string): string[] {
     let nodeList: NodeListOf<Element> | undefined;
+
     try {
       nodeList = this.codeBlock.getElement().querySelectorAll(selector);
     } catch {
       return [];
     }
+
     const signsArr: string[] = [];
+
     nodeList.forEach((node: Element) => {
       const signElement: string | null = node.getAttribute(Constants.ATTRIBUTE_SIGN_NAME);
       if (signElement) signsArr.push(signElement);
     });
+
     return signsArr;
+  }
+
+  private generateLineNumbersText(): string {
+    const numbers: string[] = [];
+    for (let i = 1; i <= Constants.LINES_COUNT; i += 1) {
+      numbers.push(i.toString());
+    }
+    return numbers.join('<br>');
+  }
+
+  private buildHTMLCode(markup: GameHTMLTag[]): void {
+    this.codeBlock.getElement().innerHTML = getHighlightedTags('<div class="table">');
+
+    markup.forEach((tag: GameHTMLTag) => {
+      const element: HTMLElement = this.createHTMLTags(tag);
+      this.codeBlock.addInnerElement(element);
+    });
+
+    this.codeBlock.getElement().insertAdjacentHTML('beforeend', getHighlightedTags('</div>'));
+  }
+
+  private createHTMLTags(markup: GameHTMLTag): HTMLElement {
+    const result: ElementCreator = new ElementCreator({
+      tag: markup.tagName,
+      classNames: markup.className ? [markup.className] : [],
+      textContent: '',
+    });
+
+    if (markup.signElement) {
+      result.setAttribute(Constants.ATTRIBUTE_SIGN_NAME, markup.signElement);
+      this.elements?.set(markup.signElement, result.getElement());
+    }
+    if (markup.idName) result.setAttribute('id', markup.idName);
+
+    if (markup.children && markup.children.length) {
+      result.getElement().innerHTML = getHighlightedTags(`<${this.getTagForHighlight(markup)}>`);
+      markup.children.forEach((child: GameHTMLTag) => {
+        const element: HTMLElement = this.createHTMLTags(child);
+        result.addInnerElement(element);
+      });
+      result.getElement().insertAdjacentHTML('beforeend', getHighlightedTags(`</${markup.tagName}>`));
+    } else {
+      result.getElement().innerHTML = getHighlightedTags(`<${this.getTagForHighlight(markup)} />`);
+    }
+
+    return result.getElement();
   }
 
   private removeSelection(): void {
@@ -94,52 +151,6 @@ export default class CodeViewerView extends View {
         }
       }
     });
-  }
-
-  public showSelectedItem(signElement: string = ''): void {
-    this.removeSelection();
-    if (!signElement) return;
-    const element = this.elements.get(signElement);
-    if (!element) return;
-    const nodes = element.children;
-    for (let i = 0; i < nodes.length; i += 1) {
-      if (nodes[i].tagName === 'SPAN') {
-        nodes[i].classList.add(CssClasses.SELECTED_LINE);
-      }
-    }
-  }
-
-  private buildHTMLCode(markup: GameHTMLTag[]): void {
-    this.codeBlock.getElement().innerHTML = getHighlightedTags('<div class="table">');
-    markup.forEach((tag: GameHTMLTag) => {
-      const element: HTMLElement = this.createHTMLTags(tag);
-      this.codeBlock.addInnerElement(element);
-    });
-    this.codeBlock.getElement().insertAdjacentHTML('beforeend', getHighlightedTags('</div>'));
-  }
-
-  private createHTMLTags(markup: GameHTMLTag): HTMLElement {
-    const params: ElementParams = {
-      tag: markup.tagName,
-      classNames: markup.className ? [markup.className] : [],
-      textContent: '',
-    };
-    const result: ElementCreator = new ElementCreator(params);
-    if (markup.signElement) result.setAttribute(Constants.ATTRIBUTE_SIGN_NAME, markup.signElement);
-    if (markup.idName) result.setAttribute('id', markup.idName);
-    if (markup.children && markup.children.length) {
-      result.getElement().innerHTML = getHighlightedTags(`<${this.getTagForHighlight(markup)}>`);
-
-      markup.children.forEach((child: GameHTMLTag) => {
-        const element: HTMLElement = this.createHTMLTags(child);
-        if (child.signElement) this.elements?.set(child.signElement, element);
-        result.addInnerElement(element);
-      });
-      result.getElement().insertAdjacentHTML('beforeend', getHighlightedTags(`</${markup.tagName}>`));
-    } else {
-      result.getElement().innerHTML = getHighlightedTags(`<${this.getTagForHighlight(markup)} />`);
-    }
-    return result.getElement();
   }
 
   private getTagForHighlight(markup: GameHTMLTag): string {
